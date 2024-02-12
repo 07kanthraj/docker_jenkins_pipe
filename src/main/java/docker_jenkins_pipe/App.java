086d0b27b29cff138d9e297802d50aed08be853b
07kanthraj <07kanthraj@gmail.com>
2024-02-12 14:43:09 +0530

    modified to trigger

--- src/main/java/docker_jenkins_pipe/App.java
+++ src/main/java/docker_jenkins_pipe/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("This is new change to test the auto build trigger");
     }
 }
+
